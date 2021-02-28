3b5d6282bba15bb6c440395917ef58372254f219
Vast342 <jcpasfield@gmail.com>
2021-02-28 18:05:52 -0500

    Initial Commit 2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20f;
+    void Update()
+    {
+        
+    }
+}
